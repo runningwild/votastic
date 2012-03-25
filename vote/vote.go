a5e8a077a6b244ce51924bc240cda3687245014b
Jonathan Wills <runningwild@gmail.com>
2012-03-24 18:34:31 -0700

    Openid authentication

--- vote/vote.go
+++ vote/vote.go
@@ -71,26 +71,39 @@ func show(w http.ResponseWriter, r *http.Request) {
   }
 }
 
+func headerLoggedIn(w http.ResponseWriter, r *http.Request, c appengine.Context, u *user.User) {
+  url, err := user.LogoutURL(c, "/")
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+  } else {
+    fmt.Fprintf(w, `Welcome, %s! (<a href="%s">sign out</a>)<br>`, u.Email, url)
+  }
+}
+
+func headerNotLoggedIn(w http.ResponseWriter, r *http.Request, c appengine.Context) {
+  fmt.Fprintf(w, "Sign in with any of the following: ")
+  providers := []struct{name,url string} {
+    {"Google", "www.google.com/accounts/o8/id" },
+    {"Yahoo", "yahoo.com" },
+    {"MyOpenID", "myopenid.com" },
+  }
+  for _, provider := range providers {
+    login_url, err := user.LoginURLFederated(c, r.URL.String(), provider.url)
+    if err != nil { continue }
+    fmt.Fprintf(w, "[<a href='%s'>%s</a>]", login_url, provider.name)
+  }
+}
+
 // If the user is not logged in, promts the user to log in.
 // If the user is logged in, adds a link at the top to let the user log out.
 // Return value indicates if the user is logged in.
 func promptLogin(w http.ResponseWriter, r *http.Request) (appengine.Context, *user.User, bool) {
   c := appengine.NewContext(r)
   u := user.Current(c)
-  if u == nil {
-    url, err := user.LoginURL(c, r.URL.String())
-    if err != nil {
-      http.Error(w, err.Error(), http.StatusInternalServerError)
-      return nil, nil, false
-    }
-    fmt.Fprintf(w, `<a href="%s">Sign in or register</a><br>`, url)
-    return nil, nil, false
-  }
-  url, err := user.LogoutURL(c, "/")
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
+  if u != nil {
+    headerLoggedIn(w, r, c, u)
     return c, u, true
   }
-  fmt.Fprintf(w, `Welcome, %s! (<a href="%s">sign out</a>)<br>`, u, url)
-  return c, u, true
+  headerNotLoggedIn(w, r, c)
+  return nil, nil, false
 }
